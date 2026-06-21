5b58a3c8dd34101525c324e130e07edd55bbcaef
Long <nghlong3004@gmail.com>
2026-06-22 03:54:10 +0700

    chore: remove unused table.tsx component

--- apps/client/src/app/layouts/AuthLayout.tsx
+++ apps/client/src/app/layouts/AuthLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet, useLocation } from "react-router-dom";
-import { motion, AnimatePresence } from "framer-motion";
+
 import { BrandLogo } from "../../components/ui/Logo";
 import { AuthDynamicCanvas } from "../../features/auth/components/AuthDynamicCanvas";
 
